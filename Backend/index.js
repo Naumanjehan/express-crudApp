68a8a66d30ce4a3cf12fc1b8510da25214d2f3e5
Naumanjehan <haristrader321@gmail.com>
2024-09-28 11:27:57 +0500

    update user

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,32 @@ app.post('/api/user', (req, res) =>{
     res.status(200).json({message : "created new user", data: newUser})
 })
 
+app.put('/api/user/:id', (req, res) =>{
+    const userId = parseInt(req.params.id)
+    const updateUser = req.body;
+    const userIndex = users.findIndex(user => user.id === userId)
+    if(userIndex === -1){
+        res.status(404).json({message : "user not found"})
+    }
+  users[userIndex] = {
+        ...users[userIndex],
+        ...updateUser
+    }
+    res.status(200).json({message : "update user successfuly", data: users[userIndex]})
+})
 
+app.delete('/api/user/:id', (req, res) =>{
+    const userId = parseInt(req.params.id)
+    const userIndex = users.findIndex(user => user.id === userId)
+
+    if(userIndex === -1){
+        res.status(404).json({message : "user not found"})
+    }
+    users.splice(userIndex, 1)
+    res.status(200).json({message : "delete user successfuly"})
+
+
+})
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
