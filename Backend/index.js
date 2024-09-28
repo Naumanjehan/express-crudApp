c93906006606c693c090eab7e6197e411cebc785
Naumanjehan <haristrader321@gmail.com>
2024-09-28 10:52:42 +0500

    install backend package

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,9 @@ const users =[
     {id: 1, name: 'nauman', email: 'naumankhan@gamil.com'},
     {id: 2, name: 'khan', email: 'khan@gamil.com'},
 ]
-
+app.get('/api/user', (req, res) =>{
+    res.status(200).json({message : "feching all user", data: users})
+})
 
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`)
