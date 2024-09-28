2799ae97f4e07ea463b696fe8df99a3a4d348c88
Naumanjehan <haristrader321@gmail.com>
2024-09-28 11:28:34 +0500

    delete user

--- Backend/index.js
+++ Backend/index.js
@@ -1,55 +1,77 @@
-
 const express = require('express')
+const cors = require('cors')
 
-const app = express();
 const PORT = 3000;
-app.use(express.json());
+const app = express();
+app.use(express.json())
+app.use(cors())
 
-const users =[
-    {id: 1, name: 'nauman', email: 'naumankhan@gamil.com'},
-    {id: 2, name: 'khan', email: 'khan1@gamil.com'},
+const users = [
+    {id: 2, name: 'nauman', email: 'nauman@gmail.com'},
+    {id: 1, name: 'najeeb', email: 'najeebkhan@gmail.com'},
 ]
+
+
+function errorHandler(err, req, res, next) {
+    res.status(err.status || 500).json({
+        message: err.message || 'Something went wrong',
+        error: true
+    })
+}
+
+// GET request  
+
 app.get('/api/user', (req, res) =>{
-    res.status(200).json({message : "feching all user", data: users})
+    res.status(200).json({ message: 'Fetched all users!', data: users })
 })
 
+// POST Requst  create new user 
 
 app.post('/api/user', (req, res) =>{
     const user = req.body;
-    const newUser = {
+    const newuser = {
         id: users.length + 1,
-        ...user,
+        ...user
     }
-    users.push(newUser)
-    res.status(200).json({message : "created new user", data: newUser})
+    users.push(newuser)
+    res.status(201).json({ message: 'new User created!', data: newuser })
 })
+
+//update users
 
 app.put('/api/user/:id', (req, res) =>{
-    const userId = parseInt(req.params.id)
-    const updateUser = req.body;
+    const userId = parseInt(req.params.id);
     const userIndex = users.findIndex(user => user.id === userId)
     if(userIndex === -1){
-        res.status(404).json({message : "user not found"})
+        res.status(404).json({message: "User not found!"})
     }
-  users[userIndex] = {
+    users[userIndex] ={
         ...users[userIndex],
-        ...updateUser
-    }
-    res.status(200).json({message : "update user successfuly", data: users[userIndex]})
+        ...req.body,
+}
+res.status(201).json({ message: 'new User created!', data: users[userIndex] })
+   
 })
 
-app.delete('/api/user/:id', (req, res) =>{
-    const userId = parseInt(req.params.id)
+// delete user 
+
+app.delete('/api/user/:id',(req, res) =>{
+    const userId = parseInt(req.params.id);
     const userIndex = users.findIndex(user => user.id === userId)
 
     if(userIndex === -1){
-        res.status(404).json({message : "user not found"})
+        res.status(404).json({message: "User not found!"})
     }
-    users.splice(userIndex, 1)
-    res.status(200).json({message : "delete user successfuly"})
-
-
+    else{
+        users.splice(userIndex, 1);
+        res.status(201).json({ message: 'users deleted successfully!'})
+        
+    }
 })
+
+app.use(errorHandler);
+
+
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
