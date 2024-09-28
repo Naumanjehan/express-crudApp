59a2dddacc0ca14e380c50cd6518fe4b8b644fe0
Naumanjehan <haristrader321@gmail.com>
2024-09-28 11:05:48 +0500

    new user crated

--- Backend/index.js
+++ Backend/index.js
@@ -13,6 +13,18 @@ app.get('/api/user', (req, res) =>{
     res.status(200).json({message : "feching all user", data: users})
 })
 
+
+app.post('/api/user', (req, res) =>{
+    const user = req.body;
+    const newUser = {
+        id: users.length + 1,
+        ...user
+    }
+    users.push(newUser)
+    res.status(200).json({message : "created new user", data: newUser})
+})
+
+
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
